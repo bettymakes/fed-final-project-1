604a96fd4cb866f416d7ece4c24769122633ce2e
Atom Wenzel <adam.wenzel@hotmail.com>
2015-07-30 22:20:40 -0400

    Burger Button - FIXED
    I FIXED IT!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,18 +6,18 @@
 
 	// BURGER MENU
 
-	// $('.burger-toggle').on('click', function(eventObject){
-	// 	console.log('this is clicked');
-	//     eventObject.preventDefault();
-	//     /*$(this).add('#burger-toggle').toggleClass('is-open');*/
-	//     $('#body-container').toggleClass('is-open');
-	// });
-
-	$('.test').on('click', function(){
-		console.log('test is clicked');
-	    // $('this').add('#burger-toggle').toggleClass('is-open');
+	$('#burger-button').on('click', function() {
+		console.log('this is clicked');
+	    // eventObject.preventDefault();
+	    /*$(this).add('#burger-toggle').toggleClass('is-open');*/
 	    $('#body-container').toggleClass('is-open');
 	});
+
+	// $('.test').on('click', function(){
+	// 	console.log('test is clicked');
+	//     // $('this').add('#burger-toggle').toggleClass('is-open');
+	//     $('#body-container').toggleClass('is-open');
+	// });
 
 	// DROP DOWN NAV
 		$("select").selectOrDie();
